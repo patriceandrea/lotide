c77975079c0fc21bb5d32f541d6ce024ed4523cb
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-07 23:27:15 +0000

    better version

--- head.js
+++ head.js
@@ -9,24 +9,10 @@ const assertEqual = function (actual, expected) {
 
 
 const head = function (array) {
-  let result = 0;
-  for (let i = 0; i < array.length - 1; i++) {
-    if (array === 0) {
-      return undefined;
-    } else {
-      result = array[i - 1];
-      //yes = console.log(result);
-
-    }
-  }
-  return result;
+  return array[0];
 };
 // TEST CODE
 
-// assertEqual(1, 1);
-// assertEqual("hi", "hi");
-// assertEqual("hi", "bye");
-// assertEqual(1, 167);
-
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5, 6, 7]), 5);
+assertEqual(head([9]), 9);
