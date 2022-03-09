8838da6647ffc203c9e13f9fef7e629bd03ff683
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-09 02:14:06 +0000

    finds if both arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !=== ${expected} `);
+  }
+};
+
+// 2 arrays as 2 parameters in function
+// for  loop : iterate every element of each array
+// compare b/w both arrays
+// if array1 = array2 --> return true
+// if !array1 = array2 --> return false
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
