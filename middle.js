4b3827f69e0186a01c582e5eba45f60def0cd264
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-09 14:43:44 +0000

    adds parenthesis

--- middle.js
+++ middle.js
@@ -23,7 +23,7 @@ const middle = function (array) {
   if (array.length <= 2) {
     return [];
   }
-  let mid = array.length / 2;
+  let mid = (array.length / 2);
   if (array.length % 2 === 1) {
     return [array[Math.floor(mid)]];
   } else {
