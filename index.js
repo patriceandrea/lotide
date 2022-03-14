23f06fd4a7edc20b4a560770db31716ec464cd70
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-14 19:28:15 +0000

    modifies the findKey function

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
-const findKeys = require('./ findKeys');
-const findKeysByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
 const letterPositions = require('./letterPositions');
 const map = require('./map');
 const takeUntil = require('./takeUntil');
@@ -26,8 +26,8 @@ module.exports = {
   countOnly,
   eqArrays,
   eqObjects,
-  findKeys,
-  findKeysByValue,
+  findKey,
+  findKeyByValue,
   letterPositions,
   map,
   takeUntil,
