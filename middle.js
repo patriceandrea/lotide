3233e63d6bb58a3637f8141a7330152f4530c5df
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-14 16:53:51 +0000

    adds module exports

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArrayEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1}} !=== ${array2} `);
-  }
-};
 
 
 const middle = function (array) {
@@ -32,9 +13,5 @@ const middle = function (array) {
   }
 };
 
-assertArrayEqual(middle([1]), []); // => []
-assertArrayEqual(middle([1, 2]), []); // => []
-assertArrayEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArrayEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArrayEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArrayEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]); // => [3, 4]
+
+module.exports = middle; 
