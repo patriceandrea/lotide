9ae46f29919d8f39abfa7a1824c273a57e59b598
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-14 16:34:39 +0000

    adds for test for assertArrayEqual

---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,9 @@
+
+const assertArrayEqual = require('../assertArrayEqual');
+const eqArrays = require('../eqArrays');
+
+assertArrayEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertArrayEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertArrayEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertArrayEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertArrayEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
