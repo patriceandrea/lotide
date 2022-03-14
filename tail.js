62afc1de592734486e63cdc268bd1ce53af4dd37
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-14 18:12:01 +0000

    updates tail function

--- tail.js
+++ tail.js
@@ -1,12 +1,7 @@
 const assertEqual = require('./assertEqual');
 
 const tail = function (array) {
-  let result = [];
-  for (let i = 1; i < array.length; i++) {
-    result = + array[i];
-
-  }
-  return result;
+  return array.slice(1);
 };
 
 module.exports = tail;
