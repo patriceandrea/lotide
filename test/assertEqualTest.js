dbdbc37e47d813900f70a694ac3275e86244fbd1
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-14 13:51:50 +0000

    assertsEqual test file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+// test/assertEqualTest.js
+const assertEqual = require('../assertEqual');
+
+
+//TEST CODE
+assertEqual(head("Lighthouse Labs", "Bootcamp"));
+assertEqual(head(1, 1));
+assertEqual(head("hi", "hi"));
+assertEqual(head("hi", "bye"));
+assertEqual(head(1, 167));
