9b5515da636b07b06ba7a0ba58bc0632204aebfb
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-15 01:15:02 +0000

    updates .gitignore to node_modules

--- countOnly.js
+++ countOnly.js
@@ -1,11 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !=== ${expected} `);
-  }
-};
 
+const assertEqual = require('./assertEqual');
 // array and object 
 // return objects: everything counted from an array 
 // array counts the numbers of same element 
@@ -30,22 +24,4 @@ const countOnly = function (allItems, itemsToCount) {
   return results;
 }
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-
-console.log(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly; 
