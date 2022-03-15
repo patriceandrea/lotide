4dd2a52d5cfbe63c907858473b2c62af22e42f56
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-15 02:05:18 +0000

    updates countOnlyTest

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -4,9 +4,6 @@ const countOnly = require('../countOnly');
 
 
 
-const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
-
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
 
 describe("#countOnly", () => {
