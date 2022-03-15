b34a1f0cd2b9a0adf57fb3655643ed63f0d6e9c5
patriceandrea <andrea_pavico98@yahoo.ca>
2022-03-15 02:00:36 +0000

    updates coutOnlyTest

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -10,22 +10,20 @@ const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "S
 
 
 describe("#countOnly", () => {
-  it("returns correct count for firstNames array ", () => {
+  it("returns correct count for firstNames array with all true values ", () => {
 
     const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
-    const actual = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-    const expected = { "Jason": 1, "Karima": undefined, "Fang": 2, "Agouhanna": 1 };
+    const actual = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": true });
+    const expected = { "Jason": 1, "Fang": 2, "Agouhanna": 1 };
     assert.deepEqual(actual, expected);
   });
-  it("returns 5 for [5, 6, 7]", () => {
-    assert.strictEqual(head([5, 6, 7]), 5);
+  it("returns correct count for firstNames array with true values and  a false value included", () => {
+    const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
+    const actual = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": false, "Agouhanna": true });
+    const expected = { "Jason": 1, "Agouhanna": 1 };
+    assert.deepEqual(actual, expected);
   });
-  it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5');
-  });
-  it("returns 9 for [9]", () => {
-    assert.strictEqual(head([9]), 9);
-  });
+
 });
 
 
